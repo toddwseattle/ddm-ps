98efcc5f2f65b731280eeaacd76fcd842db1b357
Todd Warren <twarren@ashesi.edu.gh>
2015-10-31 17:46:35 -0700

    added mdp feature as well as support dp1 and dp2

--- switchdell.ps1
+++ switchdell.ps1
@@ -1,10 +1,18 @@
 Param
           (
             [parameter(Mandatory=$false)]
-            [ValidateSet("HDMI", "mDP", "MHL","DP")]
+            [ValidateSet("HDMI", "mDP","DP2","MHL","DP1","DP")]
             [String[]]
             $Monitor="HDMI"
           ) 
+if($Monitor -eq "DP")
+{
+	$Monitor="DP1"
+}
+if($Monitor -eq "mdp")
+{
+	$Monitor="DP2"
+}
 $ddmpath="C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe"
 $setinput="/SetActiveInput"
 Write-Host "Attempting to switch to monitor" $Monitor
