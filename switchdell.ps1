6aa21556e9c776ce71d7d7a69b1af675563ebcf1
Sebastian Walter <sebwalter@gmx.net>
2019-04-25 14:14:31 +0200

    corrected param name "Monitor" to "InputSource; provided "Monitor" param to provide multi-display support

--- switchdell.ps1
+++ switchdell.ps1
@@ -1,19 +1,33 @@
 Param
-          (
-            [parameter(Mandatory=$false)]
-            [ValidateSet("HDMI", "mDP","DP2","MHL","DP1","DP")]
-            [String[]]
-            $Monitor="HDMI"
-          ) 
-if($Monitor -eq "DP")
-{
-	$Monitor="DP1"
+(
+    [parameter(Mandatory = $false)]
+    [ValidateSet("HDMI", "mDP", "DP2", "MHL", "DP1", "DP")]
+    [String[]]
+    $InputSource = "HDMI",
+    [ValidateRange(1, 63)] 
+    [Int]    
+    $Monitor = 1
+) 
+$Host.UI.RawUI.BackgroundColor = 1; 
+$Host.UI.RawUI.ForegroundColor = 15;
+Clear-Host
+if ($InputSource -eq "DP") {
+    $InputSource = "DP1"
 }
-if($Monitor -eq "mdp")
-{
-	$Monitor="DP2"
+if ($InputSource -eq "mdp") {
+    $InputSource = "DP2"
 }
-$ddmpath="C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe"
-$setinput="/SetActiveInput"
-Write-Host "Attempting to switch to monitor" $Monitor
-& $ddmpath $setinput $Monitor
+
+if ($Monitor -eq "1") {
+    $MonitorActiveInput = "/SetActiveInput"
+}
+else {
+    $MonitorActiveInput = "/$($Monitor):SetActiveInput"
+}
+
+$exit = "/exit"
+$ddmpath = "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe"
+Write-Host "Attempting to switch Display $($Monitor) to InputSource $($InputSource)..."
+& $ddmpath $MonitorActiveInput $InputSource $exit
+
+Start-Sleep 5
